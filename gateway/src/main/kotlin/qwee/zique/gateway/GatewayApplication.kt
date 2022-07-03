0e48c8ea41c5d84f87f001983fa3562731ef7e14
Nikita Kovalyov <qweezique@gmail.com>
2022-07-04 01:08:04 +0300

    add gateway service

---
+++ gateway/src/main/kotlin/qwee/zique/gateway/GatewayApplication.kt
@@ -0,0 +1,22 @@
+package qwee.zique.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+
+//    @Bean
+//    fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? {
+//        return builder.routes().route { p ->
+//                p.path("/hello")
+//                    .uri("lb://HELLO-SERVICE/hello")
+//            }.build()
+//    }
+}
+

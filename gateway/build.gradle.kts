cbe0321d0f921de489cbb82ba903af7589a93897
Nikita Kovalyov <qweezique@gmail.com>
2022-07-04 08:17:45 +0300

    change EurekaServer server port to default: 8761

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -18,20 +18,21 @@ repositories {
 extra["springCloudVersion"] = "2021.0.3"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+	implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-
+	implementation("io.netty:netty-all:4.1.77.Final")
 }
 
 dependencyManagement {
--- gateway/src/main/kotlin/qwee/zique/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/qwee/zique/gateway/GatewayApplication.kt
@@ -10,13 +10,13 @@ class GatewayApplication
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
-
-//    @Bean
-//    fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? {
-//        return builder.routes().route { p ->
-//                p.path("/hello")
-//                    .uri("lb://HELLO-SERVICE/hello")
-//            }.build()
-//    }
 }
+
+//@Bean
+//fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? {
+//    return builder.routes().route { p ->
+//        p.path("/hello")
+//            .uri("lb://HELLO-SERVICE/hello")
+//    }.build()
+//}
 

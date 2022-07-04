29cd1faf1f6378f5e5ceaa15680fa8d2a9dd52ca
Nikita Kovalyov <qweezique@gmail.com>
2022-07-04 09:00:33 +0300

    prefer ip-addresses in EurekaServer, remove in Gateway eureka.client.fetchRegistry

--- gateway/src/main/kotlin/qwee/zique/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/qwee/zique/gateway/GatewayApplication.kt
@@ -12,11 +12,4 @@ fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
 
-//@Bean
-//fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? {
-//    return builder.routes().route { p ->
-//        p.path("/hello")
-//            .uri("lb://HELLO-SERVICE/hello")
-//    }.build()
-//}
 

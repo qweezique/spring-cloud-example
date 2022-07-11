5d13dc976e0acc708848b842499f94741e230c73
Nikita Kovalyov <qweezique@gmail.com>
2022-07-11 11:30:56 +0300

    add general README.md Dockerfile eureka-server

---
+++ eureka-server/docker/run_image_eureka-server.sh
@@ -0,0 +1,4 @@
+docker run -d \
+--name=eureka-server \
+-p 0.0.0.0:8761:8761 \
+eureka-server
--- hello-service/docker/run_image_hello-service.sh
+++ hello-service/docker/run_image_hello-service.sh
@@ -2,5 +2,5 @@ docker run -d \
 --name=hello-service \
 --env="RABBITMQ_SERVICE_HOST=host.docker.internal" \
 --env="DISCOVERY_SERVICE_URL=http://host.docker.internal:8761/eureka/" \
--p 8181:8181 \
+-p 0.0.0.0:8181:8181 \
 hello-service

543460018a99892a719e613aeaa52afaa8549446
Nikita Kovalyov <qweezique@gmail.com>
2022-07-29 00:06:44 +0300

    init auth-service structure

---
+++ auth-service/src/main/kotlin/qwee/zique/authservice/AuthServiceApplication.kt
@@ -0,0 +1,11 @@
+package qwee.zique.authservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthServiceApplication>(*args)
+}
